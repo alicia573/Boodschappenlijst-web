446c76520f846dfcdb81cf592179d647b17f1127
alicia573 <54794745+alicia573@users.noreply.github.com>
2020-10-26 10:03:44 +0100

    Opdracht 3

---
+++ opdracht4.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <title>Booschappenlijst </title>
+    <link rel="stylesheet" href="">
+</head>
+    <body>
+        <form action="opdracht4.php" method="Post">
+        <label> Voeg toe:
+            <input>
+        </label>
+        <button type="submit">Verstuur</button>
+        </form>
+    <?php
+
+    ?>
+    </body>
+</html>
