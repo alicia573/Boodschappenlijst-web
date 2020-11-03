21f14a14adb246294fd9f4eb6d886e986ce2891e
alicia573 <54794745+alicia573@users.noreply.github.com>
2020-11-03 10:19:58 +0100

    opdracht 3

--- opdracht 3.php
+++ opdracht 3.php
@@ -1,13 +1,16 @@
 <?php
 
+$boodschap = array (
+    "aardappelen<img src='aardappel.jpg' style='height: 50px'> ",
+    "aardbeien <img src='aardbei.jpg' style='height: 50px'>",
+    "3 pakken melk <img src='melk.jpg' style='height: 50px'>",
+    "yoghurt <img src='yoghurt.png' style='height: 50px'>",
 
-
-
-
-$boodschap = array ("aardappelen","aardbeien"," 3 pakken melk","yoghurt");
+);
 echo "Ik moet vandaag kopen:"."<br>";
 
 foreach ($boodschap as $boodschappen) {
     echo  "<li>" . $boodschappen . "</li>";
 }
+
 ?>
